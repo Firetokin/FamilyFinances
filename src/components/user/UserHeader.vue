b28cf5e105531d8c05e3c4f98d9cc56af4d8fecc
Firetokin <1322395986@qq.com>
2020-08-11 21:26:57 +0800

    首页

---
+++ src/components/user/UserHeader.vue
@@ -0,0 +1,108 @@
+<template>
+	<!-- 头部区域 -->
+	  <el-header style="height: 120px;">
+		  <!-- 图标、名称 -->
+		  <div style="margin-bottom: 20px;">
+			  <img src="../../assets/feimg/logo_login.png" style="width: 98px; height: 98px;"/>
+			  <span>家庭理财系统</span>
+		  </div>
+		  <!-- 导航选项 -->
+		  <div class="nav_header">
+			  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+				<el-menu-item index="1">首页</el-menu-item>
+				<el-submenu index="2">
+				  <template slot="title">收支信息管理</template>
+				  <el-menu-item index="2-1">查询收入信息</el-menu-item>
+				  <el-menu-item index="2-2">添加收入信息</el-menu-item>
+				  <el-menu-item index="2-3">查询支出信息</el-menu-item>
+				  <el-menu-item index="2-4">添加支出信息</el-menu-item>
+				</el-submenu>
+				<el-menu-item index="3">查看报表</el-menu-item>
+				<el-submenu index="4">
+				  <template slot="title">个人中心</template>
+				  <el-menu-item index="4-1">个人信息</el-menu-item>
+				  <el-menu-item index="4-2">修改密码</el-menu-item>
+				  <el-menu-item index="4-3">退出登录</el-menu-item>
+				  <el-menu-item index="4-4">注销账号</el-menu-item>
+				</el-submenu>
+			  </el-menu>
+			</div>  
+	  </el-header>
+</template>
+
+<script>
+	export default {
+		name:'UserHeader',
+	    data() {
+	      return {
+	        activeIndex: '1',
+	        activeIndex2: '1'
+	      };
+	    },
+	    methods: {
+	      handleSelect(key, keyPath) {
+	        console.log(key, keyPath);
+	      }
+	    }
+	  }
+
+</script>
+
+<style lang="less">
+	.el-header{
+		width: 100%;
+		padding: 0 80px 0 50px;
+		background-color: white;
+		display: flex;
+		justify-content: space-between;
+		
+		align-items: center;
+		color: #EC7541;
+		font-size: 25px;
+		position: absolute;
+		top: 0px;
+		
+		> div {
+			display: flex;
+			align-items: center;
+			span {
+				margin-left: 20px;
+			}
+		}
+	} 
+	.el-footer {
+	    background-color: #EC7541;
+		display: flex;
+		justify-content: center;
+		line-height: 60px;
+		position: absolute;
+		bottom: 0px;
+		left: 0px;
+		width: 100%;
+	   
+	}
+	
+	.nav_header/deep/.el-menu-item{
+		font-size: 20px;
+		color: #303133;
+		padding: 0 30px;
+		cursor: pointer;
+		transition: border-color .3s,background-color .3s,color .3s;
+		box-sizing: border-box;
+		
+	}  
+	
+	.nav_header/deep/.el-submenu__title {
+		font-size: 20px;
+		color: #303133;
+		padding: 0 30px;
+		cursor: pointer;
+		transition: border-color .3s,background-color .3s,color .3s;
+		box-sizing: border-box;
+	}
+	
+	.nav_header{
+		margin-left: 500px;
+	}
+		
+</style>
--- src/components/user/UserRegister.vue
+++ src/components/user/UserRegister.vue
@@ -1,38 +1,32 @@
 <template>
 	<div class="register_container">
-		
-		<div class="reTitle_box"> 
-			欢迎注册家庭财务管理系统
-			<img src="../../assets/feimg/img_register_title.png" />
-		</div>
 		<div class="register_box">
-			
+			<div class="welcome_title">欢迎注册家庭财务管理系统 </div>
+			<div class="title_mes"> 
+				已有账号，去
+				<button @click="registertologin" class="btn_retologin">登录</button>
+			</div>
 			<el-form label-width="80px" label-position="top" class="from_register">
 				
 				<el-form-item label="用户名" >
 					<el-input ></el-input>
 				</el-form-item>
-
 			
 				<el-form-item label="密码">
 					<el-input >密码</el-input>
 				</el-form-item>
-			
 
 				<el-form-item label="工资">
 					<el-input >工资</el-input>
-				</el-form-item>
-						
+				</el-form-item>						
 			
 				<el-form-item label="年龄">
 					<el-input >年龄</el-input>
-				</el-form-item>
-			
+				</el-form-item>			
 					
 				<el-form-item label="消费额度">
 					<el-input >消费额度</el-input>
-				</el-form-item>
-			
+				</el-form-item>			
 			
 				<el-form-item label="家庭角色">
 					<el-select placeholder="请选择家庭角色">
@@ -50,15 +44,31 @@
 					<el-button type="primary" plain style="margin-right: 50px;">确认注册</el-button>
 					<el-button type="info" plain>重置</el-button>
 				</el-form-item>
-			</el-form>
-			
-		</div>
-		
+			</el-form>	
+			<div class="check_regRules">
+				<!-- `checked` 为 true 或 false -->
+				<el-checkbox v-model="checked">阅读并接受《家庭理财系统用户协议及隐私权保护声明》</el-checkbox>
+			</div>		
+		</div>		
 	</div>
 	
 </template>
 
 <script>
+		
+	export default{
+		data() {
+		      return {
+		        checked: true
+		      };
+		    },
+		methods:{
+				
+			registertologin(){
+				this.$router.push({path:'/userlogin'})
+			}
+		}
+	}
 </script>
 
 <style scoped>
@@ -75,19 +85,20 @@
 	
 	.register_box{
 		width: 500px;
-		height: 700px;
+		height: 900px;
 		background-color: aliceblue;
 		border-radius: 10px;
 		position: absolute;
 		left: 50%;
-		top: 230px;
+		top: 350px;
 		transform: translate(-50%,-30%);
+		margin-bottom: 100px;
 	}
 	.from_register{
-		position: absolute;
+		/*position: absolute;*/
 		bottom: 0px;
 		width: 100%;
-		padding: 30px 50px;
+		padding: 10px 50px;
 		box-sizing: border-box; /*不设置是默认的，框会超出表单*/
 	}
 	.from_register >>> .el-form--label-top.el-form-item__label {
@@ -97,6 +108,7 @@
 	    padding: 0 0 1px;
 	}
 	.from_register >>>.el-form-item {
+	
 	    margin-bottom: 2px;
 	}
 		
@@ -104,7 +116,26 @@
 		background-color: #FFFFFF;
 		padding: 0px 210px 0px 15px;
 	}
-	.reTitle_box{
+	.welcome_title{
+		width: 500px;
+		height: 80px;
+		margin: 50px 0 -25px 0;
+		color: #000000;
+		font-size: 30px;
+		text-align: center;
 		
 	}
+	.title_mes{
+		margin-left: 70px;
+	}
+	.btn_retologin{
+		color: blue;
+		border: none;
+		background-color: transparent;
+	}
+	.check_regRules{
+		margin-top: 30px;
+		margin-left: 70px;
+	}
+
 </style>
