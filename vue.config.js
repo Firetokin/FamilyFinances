5e68c2301efe574049859a8841c01b1322427eb3
ghgjhgjj <3441884848@qq.com>
2020-08-13 11:10:42 +0800

    更新管理端登录接口有关数据的获取-15

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
     // 它支持webPack-dev-server的所有选项
     devServer: {
     //host: "localhost",
-    port: 8081, // 端口号
+    port: 8089, // 端口号
     https: false, // https:{type:Boolean}
     //open: true, //配置自动启动浏览器
     // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
@@ -45,7 +45,7 @@ module.exports = {
     // 配置多个代理
     proxy: {
     "/family": {
-		target: "http://localhost:8080/family", // 要访问的接口域名
+		target: "http://localhost:8088/family", // 要访问的接口域名
 		ws: true, // 是否启用websockets
 		changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
 		pathRewrite: {
